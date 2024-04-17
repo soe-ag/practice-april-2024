9ecfcd9198a93eaa527fe132be713df14d0631f9
wind-ik <soeag.st@gmail.com>
2024-04-18 00:18:57 +0900

    added page num

--- pages/tmdb/testPagePoster.vue
+++ pages/tmdb/testPagePoster.vue
@@ -2,22 +2,48 @@
 const test = ref(false);
 
 const upcomingMovies = ref<MovieData[]>([]);
+const pageNum = ref(1);
 
 onMounted(async () => {
-  upcomingMovies.value = await fetchData(2);
+  upcomingMovies.value = await fetchData(pageNum.value);
 });
+
+watch(
+  pageNum,
+  async () => (upcomingMovies.value = await fetchData(pageNum.value))
+);
 </script>
 
 <template>
   <h2 class="mx-4 text-gray-6 underline italic">Posters</h2>
 
-  <div class="flex gap-2 flex-wrap">
-    <div
-      v-for="movie in upcomingMovies"
-      :key="movie.original_title"
-      :movie="movie"
-    >
-      <NuxtImg :src="`https://image.tmdb.org/t/p/w300${movie.poster_path}`" />
+  <div class="flex gap-4">
+    <div class="">
+      <InputNumber
+        v-model="pageNum"
+        showButtons
+        buttonLayout="vertical"
+        class="w-8"
+        :min="1"
+        :max="99"
+      >
+        <template #incrementbuttonicon>
+          <span class="i-mdi-gamepad-round text-red-5" />
+        </template>
+        <template #decrementbuttonicon>
+          <span class="i-mdi-minus-thick text-xl text-red-5" />
+        </template>
+      </InputNumber>
+    </div>
+
+    <div class="flex gap-2 flex-wrap">
+      <div
+        v-for="movie in upcomingMovies"
+        :key="movie.original_title"
+        :movie="movie"
+      >
+        <NuxtImg :src="`https://image.tmdb.org/t/p/w300${movie.poster_path}`" />
+      </div>
     </div>
   </div>
 </template>
